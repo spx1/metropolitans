f10d3b05dd88e9a8abb05ebd73fe23778cdad1af
Will Smith <smith.wg@gmail.com>
2025-05-27 20:58:35 -0400

    Update model.
    Include creation/update dates to track changes
    Create a separate category object
    Add category and vendor objct to Expense

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from .metropolitan import db
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, relationship
 
 class Vendor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,12 +10,25 @@ class Vendor(db.Model):
     email = db.Column(db.String(80))
     address = db.Column(db.String(100))
     comments = db.Column(db.Text)
+    created = db.Column(db.Date, nullable=False)
+    updated = db.Column(db.Date, nullable=False)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    categoryname = db.Column(db.String(50), nullable=False)
+    is_deprecated = db.Column(db.Boolean, nullable=False)
+    created = db.Column(db.Date, nullable=False)
+    updated = db.Column(db.Date, nullable=False)
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vendor_id = mapped_column(ForeignKey("vendor.id"))
-    created = db.Column(db.Date)
+    vendor = relationship("Vendor")
+    created = db.Column(db.Date, nullable=False)
+    updated = db.Column(db.Date, nullable=False)
     amount = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
     is_recurring = db.Column(db.Boolean, nullable=False)
-    category = db.Column(db.String(80), nullable=False)
+    category_id = mapped_column(ForeignKey("category.id"))
+    category = relationship("Category")
+
 
